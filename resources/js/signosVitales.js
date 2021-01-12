89126ab5e3439488f6bd6477c1c1cac82752731c
sistemas <cris.tian.ct545@gmail.com>
2021-01-12 19:17:48 -0400

    /**

--- resources/js/signosVitales.js
+++ resources/js/signosVitales.js
@@ -15,15 +15,12 @@ function calcularIMC() {
     p = $("#pesoPaciente").val();
     if (t >= 0 && p >= 0) {
         imc = Math.round(p / (t * t)).toFixed(2);
-        console.log(imc);
         if (       imc <= 18.5) {$("#icmPaciente").html(`${imc} -> Bajo Peso`);} 
         if (imc >= 18.5 && imc < 25.0) {$("#icmPaciente").html(`${imc} -> Peso Normal`);} 
         if (imc >= 25.0 && imc < 30.0) {$("#icmPaciente").html(`${imc} -> Sobre Peso`);} 
         if (imc >= 30.0 && imc < 35.0) {$("#icmPaciente").html(`${imc} -> Obesidad grado I`);} 
         if (imc >= 35.0 && imc < 40.0) {$("#icmPaciente").html(`${imc} -> Obesidad grado II`);} 
         if (40.0 < imc       ) {$("#icmPaciente").html(`${imc} -> Obesidad grado III`);} 
-         
-        
     } else {
         $("#icmPaciente").html("");
     }
