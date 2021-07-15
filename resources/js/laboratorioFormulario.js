200d53db187229c5069db5db4702398221ee5a39
Tians <cris.tian.ct545@gmail.com>
2021-07-15 01:58:46 -0400

    /**/

--- resources/js/laboratorioFormulario.js
+++ resources/js/laboratorioFormulario.js
@@ -216,7 +216,7 @@ function contentFormLab(tipo) {
       <hr>
       <button class="btn btn-theme-inverse" type="submit">Registrar</button>
     `;
-    //    *Grupo Sanguineo
+    //    *Serologia
     d = `
       <h3 align="center">Serologia</h3><br>
           <div class="row">
@@ -323,4 +323,421 @@ function contentFormLab(tipo) {
         default:
             break;
     }
+}
+
+function mostrarVista(dat) {
+    console.log(dat);
+    switch (dat.tipo) {
+        case 1:
+            console.log(dat.data[5].name);
+            var a1 = `
+            <div class="col-lg-12" id="View_BioClin">
+                <h3 align="center">-----Bioquimica Clinica-----</h3>
+                <br />
+                <div class="row">
+                    <div class="col-lg-6">
+                    <h4>Perfil Metabolico</h4>
+                    <br />
+                    <table
+                        class="table-striped"
+                        style="border-color: black"
+                        width="100%"
+                        border="2"
+                        cellpadding="0"
+                        cellspacing="2"
+                    >
+                        <tr>
+                        <td>Glucemia</td>
+                        <td width="10%">${dat.data[0].value}</td>
+                        <td style="font-size: x-small" align="right">70-110 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Ac urico</td>
+                        <td width="10%">${dat.data[1].value}</td>
+                        <td style="font-size: x-small" align="right">2.0-6.0 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Proteinas totales</td>
+                        <td width="10%">${dat.data[2].value}</td>
+                        <td style="font-size: x-small" align="right">6.2-8.5 g/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Albumina</td>
+                        <td width="10%">${dat.data[3].value}</td>
+                        <td style="font-size: x-small" align="right">3.5-5.3 g/dL</td>
+                        </tr>
+                    </table>
+                    <hr />
+                    <h4>Perfil Renal</h4>
+                    <br />
+                    <table class="table-bordered" width="100%">
+                        <tr>
+                        <td>Creatinina</td>
+                        <td>${dat.data[4].value}</td>
+                        <td style="font-size: x-small" align="right">0.6 - 1.4 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Nitrogeno ureico</td>
+                        <td>${dat.data[5].value}</td>
+                        <td style="font-size: x-small" align="right">7.0 - 25 mg&dL</td>
+                        </tr>
+                        <tr>
+                        <td>Urea</td>
+                        <td>${dat.data[6].value}</td>
+                        <td style="font-size: x-small" align="right">15 - 45 mg/dL</td>
+                        </tr>
+                    </table>
+                    <hr />
+                    <h4>Perfil lipidico</h4>
+                    <table class="table-bordered" width="100%">
+                        <tr>
+                        <td>Colesterol</td>
+                        <td>${dat.data[7].value}</td>
+                        <td style="font-size: x-small" align="right">< 200mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Triglicericos</td>
+                        <td>${dat.data[8].value}</td>
+                        <td style="font-size: x-small" align="right">30 -150 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>HDL-COL</td>
+                        <td>${dat.data[9].value}</td>
+                        <td style="font-size: x-small" align="right">30-85 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>LDL-COL</td>
+                        <td>${dat.data[10].value}</td>
+                        <td style="font-size: x-small" align="right">< 155 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>VLDL</td>
+                        <td>${dat.data[11].value}</td>
+                        <td style="font-size: x-small" align="right">< 40mg/dL</td>
+                        </tr>
+                    </table>
+                    </div>
+                    <div class="col-lg-6">
+                    <h4>Perfil Metabolico</h4>
+                    <br />
+                    <table
+                        class="table-striped"
+                        style="border-color: black"
+                        width="100%"
+                        border="2"
+                        cellpadding="0"
+                        cellspacing="2"
+                    >
+                        <tr>
+                        <td>Glucemia</td>
+                        <td width="10%">${dat.data[12].value}</td>
+                        <td style="font-size: x-small" align="right">70-110 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Ac urico</td>
+                        <td width="10%">${dat.data[13].value}</td>
+                        <td style="font-size: x-small" align="right">2.0-6.0 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Proteinas totales</td>
+                        <td width="10%">${dat.data[14].value}</td>
+                        <td style="font-size: x-small" align="right">6.2-8.5 g/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Albumina</td>
+                        <td width="10%">${dat.data[15].value}</td>
+                        <td style="font-size: x-small" align="right">3.5-5.3 g/dL</td>
+                        </tr>
+                    </table>
+                    <hr />
+                    <h4>Perfil Renal</h4>
+                    <table class="table-bordered" width="100%">
+                        <tr>
+                        <td>Creatinina</td>
+                        <td>${dat.data[16].value}</td>
+                        <td style="font-size: x-small" align="right">0.6 - 1.4 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Nitrogeno ureico</td>
+                        <td>${dat.data[17].value}</td>
+                        <td style="font-size: x-small" align="right">7.0 - 25 mg&dL</td>
+                        </tr>
+                        <tr>
+                        <td>Urea</td>
+                        <td>${dat.data[18].value}</td>
+                        <td style="font-size: x-small" align="right">15 - 45 mg/dL</td>
+                        </tr>
+                    </table>
+                    <hr />
+                    <h4>Perfil lipidico</h4>
+                    <table class="table-bordered" width="100%">
+                        <tr>
+                        <td>Colesterol</td>
+                        <td>${dat.data[19].value}</td>
+                        <td style="font-size: x-small" align="right">< 200mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>Triglicericos</td>
+                        <td>${dat.data[20].value}</td>
+                        <td style="font-size: x-small" align="right">30 -150 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>HDL-COL</td>
+                        <td>${dat.data[21].value}</td>
+                        <td style="font-size: x-small" align="right">30-85 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>LDL-COL</td>
+                        <td>${dat.data[22].value}</td>
+                        <td style="font-size: x-small" align="right">< 155 mg/dL</td>
+                        </tr>
+                        <tr>
+                        <td>VLDL</td>
+                        <td>${dat.data[23].value}</td>
+                        <td style="font-size: x-small" align="right">< 40mg/dL</td>
+                        </tr>
+                    </table>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-danger btn-sm" onclick="eraseFormLab('View_BioClin','1')"> <i class="fa fa-eraser"></i> </button>
+                <h3 align="center">----- // -----</h3>
+                <br />
+            </div>
+            `;
+
+            break;
+
+        case 2:
+            a1 = `
+            <div class="col-lg-12" id="View_Coag">
+            <h3 align="center">-----Coagulograma-----</h3>
+            <table class="table-striped" style="border-color: black;" width="100%" border="2" cellpadding="0" cellspacing="2">
+                <tr>
+                    <td>tiempo de coagulación</td>
+                    <td width="">${dat.data[0].value} min</td>
+                    <td style="font-size: x-small;" align="right">6 - 12 min</td>
+                </tr>
+                <tr>
+                    <td>Tiempo de sangria</td>
+                    <td width="">${dat.data[1].value} min</td>
+                    <td style="font-size: x-small;" align="right">1 - 3 min</td>
+                </tr>
+                <tr>
+                    <td>Tiempo de protombina</td>
+                    <td width="">${dat.data[2].value} segundos</td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Actividad protombinica</td>
+                    <td width="">${dat.data[3].value}</td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>INR</td>
+                    <td width="">${dat.data[4].value}</td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+            </table>
+            <br>
+            <h4>Control</h4>
+            <p>
+                Tiempo de protrombina => 13 seg <br>
+                Actividad protrombinica => 100% <br>
+                INR =>
+            </p>
+            <button type="button" class="btn btn-danger btn-sm" onclick="eraseFormLab('View_Coag','2')"> <i class="fa fa-eraser"></i> </button>
+            <h3 align="center">----- // -----</h3><br>
+        </div>
+            `;
+            break;
+
+        case 3:
+            a1 = `
+            <div class="col-lg-12" id="View_Coag">
+            <h3 align="center">-----Coagulograma-----</h3>
+            <table class="table-striped" style="border-color: black;" width="100%" border="2" cellpadding="0" cellspacing="2">
+                <tr>
+                    <td>Globulos rojos</td>
+                    <td width="">${dat.data[0].value} min</td>
+                    <td style="font-size: x-small;" align="right">millones/mm3</td>
+                </tr>
+                <tr>
+                    <td>Hemoglobina</td>
+                    <td width="">${dat.data[1].value} min</td>
+                    <td style="font-size: x-small;" align="right">g/dL</td>
+                </tr>
+                <tr>
+                    <td>Hematocrito</td>
+                    <td width="">${dat.data[2].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>VES</td>
+                    <td width="">${dat.data[3].value} min</td>
+                    <td style="font-size: x-small;" align="right">mm/hora</td>
+                </tr>
+                <tr>
+                    <td>Leucocitos</td>
+                    <td width="">${dat.data[4].value} min</td>
+                    <td style="fondt-size: x-small;" align="right">x/mm3</td>
+                </tr>
+                <tr>
+                    <td>Cayados</td>
+                    <td width="">${dat.data[5].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>Neutrofilos</td>
+                    <td width="">${dat.data[6].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>Eusinófilos</td>
+                    <td width="">${dat.data[7].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>Basófilos</td>
+                    <td width="">${dat.data[8].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>Linfositos</td>
+                    <td width="">${dat.data[9].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                <tr>
+                    <td>Monocitos</td>
+                    <td width="">${dat.data[10].value} min</td>
+                    <td style="font-size: x-small;" align="right">%</td>
+                </tr>
+                
+            </table>
+            <br>
+            <h4>Control</h4>
+            <p>
+                Tiempo de protrombina => 13 seg <br>
+                Actividad protrombinica => 100% <br>
+                INR =>
+            </p>
+            <button type="button" class="btn btn-danger btn-sm" onclick="eraseFormLab('View_Coag','2')"> <i class="fa fa-eraser"></i> </button>
+            <h3 align="center">----- // -----</h3><br>
+        </div>
+            `;
+            break;
+
+        case 4:
+            // *Serologia
+            a1 = `
+            <div class="col-lg-12" id="View_Coag">
+            <h3 align="center">-----Serologia-----</h3>
+            <table class="table-striped" style="border-color: black;" width="100%" border="2" cellpadding="0" cellspacing="2">
+                <tr>
+                    <td>Grupo Sanguineo</td>
+                    <td width="">${dat.data[0].value}</td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Factor RH</td>
+                    <td width="">${dat.data[1].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>R. P. R.</td>
+                    <td width="">${dat.data[2].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>P. C. R.</td>
+                    <td width="">${dat.data[3].value} </td>
+                    <td style="font-size: x-small;" align="right">mg/L  12mg/L</td>
+                </tr>
+                <tr>
+                    <td>F. Reumatoide</td>
+                    <td width="">${dat.data[4].value} </td>
+                    <td style="fondt-size: x-small;" align="right">UI/mL20-30 UI/mL</td>
+                </tr>
+                <tr>
+                    <td>A.S.T.O.</td>
+                    <td width="">${dat.data[5].value} </td>
+                    <td style="font-size: x-small;" align="right">UI/mL a 200UI/mL</td>
+                </tr>
+                <tr>
+                <h4>Antigenos Febriles (WIDAL)</h4>
+                </tr>
+                <tr>
+                    <td>Eberth "O"</td>
+                    <td width="">${dat.data[6].value}</td>
+                    <td style="font-size: x-small;" align="right">1/80</td>
+                </tr>
+                <tr>
+                    <td>Eberth "H"</td>
+                    <td width="">${dat.data[7].value} </td>
+                    <td style="font-size: x-small;" align="right">1/40</td>
+                </tr>
+                <tr>
+                    <td>Paratifus "A"</td>
+                    <td width="">${dat.data[8].value} </td>
+                    <td style="font-size: x-small;" align="right">1/40</td>
+                </tr>
+                <tr>
+                    <td>Pratifus "B"</td>
+                    <td width="">${dat.data[9].value} </td>
+                    <td style="font-size: x-small;" align="right">1/80</td>
+                </tr>
+                <tr>
+                <h4>Prueba Rapida de VIH<h4>
+                </tr>
+                <tr>
+                    <td>Muestra</td>
+                    <td width="">${dat.data[10].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Examen Solicitado</td>
+                    <td width="">${dat.data[11].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Resultado</td>
+                    <td width="">${dat.data[12].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                <h4>Prueba de Embarazo<h4>
+                </tr>
+                <tr>
+                    <td>F.U.M.</td>
+                    <td width="">${dat.data[13].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Muestra</td>
+                    <td width="">${dat.data[14].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                <tr>
+                    <td>Resultado</td>
+                    <td width="">${dat.data[15].value} </td>
+                    <td style="font-size: x-small;" align="right"></td>
+                </tr>
+                
+            </table>
+            <br>
+            <h4>Control</h4>
+            <p>
+                Tiempo de protrombina => 13 seg <br>
+                Actividad protrombinica => 100% <br>
+                INR =>
+            </p>
+            <button type="button" class="btn btn-danger btn-sm" onclick="eraseFormLab('View_Coag','2')"> <i class="fa fa-eraser"></i> </button>
+            <h3 align="center">----- // -----</h3><br>
+        </div>
+            `;
+
+            break;
+        default:
+            break;
+    }
+    $("#sect_result_lab").append(a1);
 }
