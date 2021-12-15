1df698e7dd160c8a70948607ebaacf8613610b9c
tian <cris.tian.ct545@gmail.com>
2021-12-15 00:29:09 -0400

    /********

--- app/Http/Controllers/HistoriaClinciaController.php
+++ app/Http/Controllers/HistoriaClinciaController.php
@@ -52,26 +52,33 @@ class HistoriaClinciaController extends Controller
      */
     public function showHCL(Request $request)
     {
-        $paciente=pacientes::where('pa_id',$request->input('id'))->first();
-        $fechnac=$paciente->pa_fechnac;
-        $fechnac=Carbon::parse($fechnac)->format('d-m-Y');
-        $receta=recetarioM::where('id_Paciente',$request->input('id'))
-                ->join('Users as u','u.id','=','recetario_m_s.id_usuMed')
-                ->select('recetario_m_s.*')
-                ->addSelect('u.usu_nombre','u.usu_appaterno','u.usu_apmaterno')
-                ->get();
-        $vista= view('paciente.hcl')
-                ->with('paciente',$paciente)
-                ->with('fechnac',$fechnac) 
-                ->with('receta',$receta); 
+        $paciente = pacientes::where('pa_id', $request->input('id'))->first();
+        $fechnac = $paciente->pa_fechnac;
+        $fechnac = Carbon::parse($fechnac)->format('d-m-Y');
+        $receta = recetarioM::where('id_Paciente', $request->input('id'))
+            ->join('Users as u', 'u.id', '=', 'recetario_m_s.id_usuMed')
+            ->select('recetario_m_s.*')
+            ->addSelect('u.usu_nombre', 'u.usu_appaterno', 'u.usu_apmaterno')
+            ->get();
+        $vista = view('paciente.hcl')
+            ->with('paciente', $paciente)
+            ->with('fechnac', $fechnac)
+            ->with('receta', $receta);
         return $vista;
     }
 
     public function showSigVi(Request $request)
     {
-        $sv=signosvitales::where('sv_idPaciente',$request->input('id'))->latest('id')->first();
-        
-        return $sv;
+        $sv = signosvitales::where('sv_idPaciente', $request->input('id'))->latest('id')->first();
+        $date = $sv['created_at'];
+        $date = Carbon::parse($date)->format('Y-m-d');
+        if ($date == Carbon::now()->format('Y-m-d')) {
+            $estado = 1;
+        } else {
+            $estado = 0;
+        }
+        return $date;
+        return ['sv' => $sv, 'date' => $date, 'estado' => $estado];
     }
 
     public function edit(historiaClincia $historiaClincia)
@@ -95,21 +102,21 @@ class HistoriaClinciaController extends Controller
         // ->select('cit_prevs.*','pacientes.pa_nombre','pacientes.pa_appaterno')
         // ->get();
 
-        $resp=atencion::where('atencion.ate_estAteMed',0)
-        ->Where('ate_med',Auth::user()->id)
-        ->Where('ate_pago','cancelado')
-        ->whereDate('atencion.created_at',Carbon::now()->format('Y-m-d'))
-        ->join('pacientes as pa','pa.pa_id','atencion.pa_id')
-        ->select('atencion.id','atencion.pa_id','atencion.ate_procedimiento','pa.pa_nombre','pa.pa_appaterno')->limit('10')->get();
+        $resp = atencion::where('atencion.ate_estAteMed', 0)
+            ->Where('ate_med', Auth::user()->id)
+            ->Where('ate_pago', 'cancelado')
+            ->whereDate('atencion.created_at', Carbon::now()->format('Y-m-d'))
+            ->join('pacientes as pa', 'pa.pa_id', 'atencion.pa_id')
+            ->select('atencion.id', 'atencion.pa_id', 'atencion.ate_procedimiento', 'pa.pa_nombre', 'pa.pa_appaterno')->limit('10')->get();
         return $resp;
     }
     public function nroPacienteCola()
     {
-        return citPrev::where('cp_med',17)->where('cp_estado',1)->count();
+        return citPrev::where('cp_med', 17)->where('cp_estado', 1)->count();
     }
     public function concluirAte(Request $request)
     {
-        $res=atencion::where('id',$request->input('id'))->update(['ate_estAteMed'=>1]); 
-        return ["estado"=>$res,'id'=>$request->input('id')];       
+        $res = atencion::where('id', $request->input('id'))->update(['ate_estAteMed' => 1]);
+        return ["estado" => $res, 'id' => $request->input('id')];
     }
 }
